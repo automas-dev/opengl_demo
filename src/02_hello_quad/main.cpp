6dd7a3d8ca8b8a3fb55c051cab808ffcf43928ab
Thomas Harrison <twh2898@vt.edu>
2022-02-11 22:28:38 -0500

    Added 02_hello_quad

---
+++ src/02_hello_quad/main.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#include <GL/glew.h>
+
+#include <SFML/Graphics.hpp>
+#include <glm/glm.hpp>
+
+static const char * vertexShaderSource = R"(
+#version 330 core
+layout (location = 0) in vec3 aPos;
+void main() {
+    gl_Position = vec4(aPos, 1.0);
+})";
+
+static const char * fragmentShaderSource = R"(
+#version 330 core
+out vec4 FragColor;
+void main() {
+    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+})";
+
+static GLuint loadShader() {
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, 0);
+    glCompileShader(vertexShader);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, 0, infoLog);
+        cerr << "Error vertex shader compile: " << infoLog << endl;
+        return 0;
+    }
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, 0);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, 0, infoLog);
+        cerr << "Error fragment shader compile: " << infoLog << endl;
+        return 0;
+    }
+
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+    glLinkProgram(program);
+    glGetProgramiv(program, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(program, 512, 0, infoLog);
+        cerr << "Error shader link: " << infoLog << endl;
+        return 0;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
+}
+
+void draw_array(const std::vector<glm::vec3> & vertices, GLenum mode) {
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3),
+                          &vertices[0].x);
+
+    glDrawArrays(mode, 0, vertices.size());
+
+    glDisableVertexAttribArray(0);
+}
+
+void draw_quad(const glm::vec2 & pos, const glm::vec2 & size) {
+    std::vector<glm::vec3> vertices {
+        glm::vec3(pos.x, pos.y + size.y, 0.0),
+        glm::vec3(pos.x, pos.y, 0.0),
+        glm::vec3(pos.x + size.x, pos.y, 0.0),
+
+        glm::vec3(pos.x, pos.y + size.y, 0.0),
+        glm::vec3(pos.x + size.x, pos.y, 0.0),
+        glm::vec3(pos.x + size.x, pos.y + size.y, 0.0),
+    };
+
+    draw_array(vertices, GL_TRIANGLES);
+}
+
+int main() {
+    const sf::ContextSettings settings(24, 1, 8, 3, 0);
+    sf::RenderWindow window(sf::VideoMode(800, 600),
+                            "Hello Triangle",
+                            sf::Style::Default,
+                            settings);
+    window.setVerticalSyncEnabled(true);
+    window.setFramerateLimit(60);
+    window.setActive();
+    window.setKeyRepeatEnabled(false);
+
+    // glewExperimental = true;
+    GLenum err = glewInit();
+    if (err != GLEW_OK) {
+        cerr << "glewInit failed: " << glewGetErrorString(err);
+        return 1;
+    }
+
+    GLuint program = loadShader();
+    if (!program)
+        return 1;
+
+    const float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // Bottom Left
+        0.5f,  -0.5f, 0.0f, // Bottom Right
+        0.0f,  0.5f,  0.0f // Top Center
+    };
+
+    // uncomment this call to draw in wireframe polygons.
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape)
+                        window.close();
+                    break;
+                case sf::Event::Resized: {
+                    sf::FloatRect visibleArea(0, 0, event.size.width,
+                                              event.size.height);
+                    window.setView(sf::View(visibleArea));
+                    glViewport(0, 0, event.size.width, event.size.height);
+                } break;
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(program);
+        draw_quad({-0.5, -0.5}, {1, 1});
+
+        window.display();
+    }
+
+    glDeleteProgram(program);
+
+    window.close();
+
+    return 0;
+}
